c9cb96b715991336934eed3ab0b8d24345f842b3
SamMaxwell <maxwellsamjm@gmail.com>
2022-02-04 15:10:32 +0100

    modif API test

--- Flask/API_test_1/apply.py
+++ Flask/API_test_1/apply.py
@@ -7,8 +7,9 @@ apply = Flask(__name__)
 
 @apply.route("/")
 def all_data():
-    js = data.df.to_json(orient="records")
-    # return json.loads(json.data.df)
-    return "ok"
+    # js = data.df.to_json(orient="records")
+    js = data.df.to_json(orient="split")
+    # return json.loads(json.dumps(js))
+    # return jsonify(json.loads(js))
+    return json.loads(js)
 
-apply.run()
