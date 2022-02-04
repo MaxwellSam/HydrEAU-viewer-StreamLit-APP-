616f3f4a5fd265b31770b08848893db82c48a0f7
Maxwell <sam.maxwell@suez.com>
2022-02-04 10:44:09 +0100

    first commit

---
+++ Flask/app.py
@@ -0,0 +1,33 @@
+from logging import exception
+# from flask import Flask, render_template
+from flask import *
+import pandas as pd
+import json
+import localData
+
+myString = "this is a string"
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/data")
+def data():
+    l = []
+    l.append(1)
+    l.append(2)
+    return {"list":l}
+
+@app.route("/txt")
+def txt():
+    data()
+    return localData.dataTXT
+
+@app.route("/dataframe")
+def dataframe():
+    return render_template('view.html', tables=[localData.dataCSV.to_html(classes='data')], titles=localData.dataCSV.columns.values)
+# methods
+# myString = "this is a string"
+# txt(myString)
---
+++ Flask/localData.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+# methods 
+## load local file
+
+def readTXT(path):
+    try:
+        fileObject = open(path)
+        txt = fileObject.read()
+        return txt
+    except OSError:
+        print('cannot open', path)
+
+def readCSV(path):
+    return pd.read_csv(path)
+
+# Test 1: text file 
+
+dataTXT = readTXT("./test.txt")
+print(dataTXT)
+
+# Test 2: csv file
+
+dataCSV = readCSV("./water_potability.csv")
+print(dataCSV)
+
+
+
+
