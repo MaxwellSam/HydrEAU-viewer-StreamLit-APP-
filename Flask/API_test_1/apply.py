1b2ee0d9a3bcc1f27cbe1d612416e5cf5c8e1677
Your Name <you@example.com>
2022-02-06 17:15:16 +0100

    modif apply api

--- Flask/API_test_1/apply.py
+++ Flask/API_test_1/apply.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import pandas as pd
 import json
 import data
@@ -7,9 +7,25 @@ apply = Flask(__name__)
 
 @apply.route("/")
 def all_data():
+    # result = data.df.to_json(orient="columns")
+    result = data.df.to_json(orient="split")
     # js = data.df.to_json(orient="records")
     js = data.df.to_json(orient="split")
     # return json.loads(json.dumps(js))
     # return jsonify(json.loads(js))
-    return json.loads(js)
+    # return json.loads(js)
+    # return json.loads(result)
+    return json.loads(result)
 
+@apply.route("/REGION/<string:region_name>")
+def region(region_name):
+    if (region_name not in data.df["region"]):
+        return "region not in dataframe"
+    else:
+        df_region = data.filter_region(region_name, data.df)
+        result = df_region.to_json(orient="split")
+        return json.loads(result)
+
+
+if __name__ == "__main__":
+    apply.run(debug=True)
--- Flask/API_test_1/data.py
+++ Flask/API_test_1/data.py
@@ -4,5 +4,11 @@ import pandas as pd
 # Variables 
 filePATH = "./data_2020-04.csv"
 
-df = pd.read_csv(filePATH, sep=',', index_col=0)
+df = pd.read_csv(filePATH, sep=',')
+# df = pd.read_csv(filePATH, sep=',', index_col=0)
 # print(df)
+
+## Filter region 
+
+def filter_region(region_name, dataframe):
+    return df[df["region"]=="Bretagne"]
