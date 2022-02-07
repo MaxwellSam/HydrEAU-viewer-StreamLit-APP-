0b951327575d0263dbb3d7f69cd4dcb804a3bf10
SamMaxwell <maxwellsamjm@gmail.com>
2022-02-07 11:16:41 +0100

    add branch API and modif

--- Flask/API_test_1/apply.py
+++ Flask/API_test_1/apply.py
@@ -20,17 +20,26 @@ def all_data():
 
 @apply.route("/groupby")
 def groupby():
+    """
+    return dataframe grouped by as json
+    """
     return json.loads(data.df_groupby.to_json())
 
 @apply.route("/index")
 def index():
+    """
+    return dataframe indexed as json 
+    """
     return json.loads(data.df_index.to_json(orient="index"))
 
 @apply.route("/REGION/<string:region_name>")
 def region(region_name):
-    if (region_name not in data.df["region"]):
-        return "region not in dataframe"
-    else:
+    """
+    return dataframe of `region_name` as json
+    """
+    if (region_name not in data.df.values):
+        return {'message':'bad request, '+region_name+' not in df'}, 400
+    else :
         df_region = data.filter_region(region_name, data.df)
         result = df_region.to_json(orient="split")
         return json.loads(result)
