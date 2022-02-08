e822e21f1e6330c0de83ea23d9077810e7a62a93
SamMaxwell <maxwellsamjm@gmail.com>
2022-02-08 11:36:03 +0100

    modif main

--- Flask/API_test_1/main.py
+++ Flask/API_test_1/main.py
@@ -4,13 +4,13 @@ import json
 
 # Methods
 
-def url_to_dataframe(url):
+def url_region_to_dataframe(url, region):
     """
     convert url reponse to json and convert json to return pandas dataframe 
     """ 
     resp = requests.get(url)
     data_json = json.loads(resp.text)
-    return pd.DataFrame.from_dict(data_json, orient="index").set_index(["region", "day"])
+    return pd.DataFrame.from_dict(data_json[region], orient="index")
 
 """
 ===============================================================
@@ -22,11 +22,12 @@ def url_to_dataframe(url):
 # Test 1: Get Bretagne's data 
 
 url_Bretagne = "http://127.0.0.1:5000/REGION/Bretagne"
-df_Bretagne = url_to_dataframe(url_Bretagne)
+df_Bretagne = url_region_to_dataframe(url_Bretagne, "Bretagne")
 print(df_Bretagne)
 
 # Test 2: Get Nouvelle-Aquitaine's data
 
 url_N_Aquitaine = "http://127.0.0.1:5000/REGION/Nouvelle-Aquitaine"
-df_Aquitaine = url_to_dataframe(url_N_Aquitaine)
+df_Aquitaine = url_region_to_dataframe(url_N_Aquitaine, "Nouvelle-Aquitaine")
 print(df_Aquitaine)
+
